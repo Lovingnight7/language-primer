2df07c5dd1da768c89474742085c891d89eb3013
Shuo YANG <z5254409@ad.unsw.edu.au>
2025-03-07 13:29:16 +1100

    update files

--- server/server.py
+++ server/server.py
@@ -4,6 +4,27 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+groups = [
+    {
+        "id": 1,
+        "groupName": "Group 1",
+        "members": [1, 2, 3],
+    },
+    {
+        "id": 2,
+        "groupName": "Group 2",
+        "members": [4, 5],
+    },
+]
+
+students = [
+    {"id": 1, "name": "Alice"},
+    {"id": 2, "name": "Bob"},
+    {"id": 3, "name": "Charlie"},
+    {"id": 4, "name": "David"},
+    {"id": 5, "name": "Eve"},
+]
+
 @app.route('/api/groups', methods=['GET'])
 def get_groups():
     """
@@ -11,18 +32,7 @@ def get_groups():
     return: Array of group objects
     """
     # TODO: (sample response below)
-    return jsonify([
-        {
-            "id": 1,
-            "groupName": "Group 1",
-            "members": [1, 2, 3],
-        },
-        {
-            "id": 2,
-            "groupName": "Group 2",
-            "members": [4, 5],
-        },
-    ])
+    return jsonify(groups)
 
 @app.route('/api/students', methods=['GET'])
 def get_students():
@@ -31,13 +41,7 @@ def get_students():
     return: Array of student objects
     """
     # TODO: (sample response below)
-    return jsonify([
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-        {"id": 3, "name": "Charlie"},
-        {"id": 4, "name": "David"},
-        {"id": 5, "name": "Eve"},
-    ])
+    return jsonify(students)
 
 @app.route('/api/groups', methods=['POST'])
 def create_group():
@@ -54,11 +58,37 @@ def create_group():
     group_members = group_data.get("members")
     
     # TODO: implement storage of a new group and return their info (sample response below)
-
+    global groups
+    global students
+    new_id = 1
+    members = []
+    if groups:
+        new_id = groups[-1]["id"] + 1
+    for member_name in group_members:
+        stu_id = -1
+        for student in students:
+            if student['name'] == member_name:
+                stu_id = student['id']
+                break
+        if stu_id != -1 :
+            members.append(stu_id)
+        else:
+            if students:
+                stu_id = students[-1]['id'] + 1
+                students.append({"id": stu_id, "name": member_name})
+                members.append(stu_id)
+            else:
+                students.append({"id": 1, "name": member_name})
+                members.append(1)
+    groups.append({
+        "id": new_id,
+        "groupName": group_name,
+        "members": members,
+    })
     return jsonify({
-        "id": 3,
-        "groupName": "New Group",
-        "members": [1, 2],
+        "id": new_id,
+        "groupName": group_name,
+        "members": members,
     }), 201
 
 @app.route('/api/groups/<int:group_id>', methods=['DELETE'])
@@ -69,7 +99,8 @@ def delete_group(group_id):
     return: Empty response with status code 204
     """
     # TODO: (delete the group with the specified id)
-
+    global groups
+    groups = [group for group in groups if group["id"] != group_id]
     return '', 204  # Return 204 (do not modify this line)
 
 @app.route('/api/groups/<int:group_id>', methods=['GET'])
@@ -80,14 +111,19 @@ def get_group(group_id):
     return: The group object with member details
     """
     # TODO: (sample response below)
+    global groups
+    global students
+    group = [group for group in groups if group["id"] == group_id]
+    if len(group) == 0:
+        abort(404, "Group not found")
+    members = []
+    for stu_id in group[0]['members']:
+        stu = [stu for stu in students if stu["id"] == stu_id][0]
+        members.append(stu)
     return jsonify({
-        "id": 1,
-        "groupName": "Group 1",
-        "members": [
-            {"id": 1, "name": "Alice"},
-            {"id": 2, "name": "Bob"},
-            {"id": 3, "name": "Charlie"},
-        ],
+        "id": group[0]['id'],
+        "groupName": group[0]['groupName'],
+        "members": members,
     })
     # TODO:
     # if group id isn't valid:
